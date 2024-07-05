75bbe0896db592a1810ff817d8251ba7247a0cb2
Darkoahh <yaakwachie@gmail.com>
2024-07-05 15:04:37 +0000

    my first practicals

---
+++ new_work.py
@@ -0,0 +1,74 @@
+print("helllo")
+print("darkoa")
+print("yaa darkoa kwachie")
+name=("tom")
+salary=4500
+print(name)
+firstname=("yaa ")
+secondname=("darkoa ")
+thirdname=("kwachie")
+print(firstname+secondname+thirdname)
+
+fname="Emmanuel "
+lname="nyarko"
+print("my full name is "+ fname + lname)
+a=8789
+b=6
+print("the sum of",a ,"and",b ,"is",a+b)
+
+f=10
+y=300
+r=567
+print("the product of", f, y, "and", r, "is " , f*y*r)
+mname="Yaa "
+pname="Darkoa"
+print("my name is " +mname+ pname)
+
+print("hgdhdhhvshfjfjf \n"
+      "hhfhfjjjddjjf \n"
+      "jdbjdjdjdjd \n"
+      "djcjdjcjd \n"
+      "dkdckjdkckd ")
+
+#firname = input("enter your first name")
+#secname = input("enter your second name")
+
+#print("my full name is " + firname + " " + secname)
+#hometown= input("enter your hometown")
+#region= input("enter your region")
+#print("i come from" +hometown + "" +region)
+#number1= int(input ("ENTER NUMBER1"))
+#number2= int(input("ENTER NUMBER2"))
+#print("the sum of ", number1, "and"+ "" ,number2, "is" ,number1+number2)
+#no3=int(input('enter no3'))
+#no8=int(input("enter no8"))
+#print("the product of",no3, "and" + "" , no8, "is",no3*no8)
+#no5=int(input("enter no5"))
+#no1=int(input("enter no1"))
+#no3=int(input("enter no3"))
+#average= (no5+no1+no3)/3
+#print("the average of",no5,no1, "and"+"",no3, "is", average)
+#age= int(input("enter your age"))
+#if age < 13:
+#      print("he is a child")
+#elif 13 <= age <= 19:
+#      print("he is a teenager")
+#else:
+#     print("he is an adult")
+
+#number=int(input("enter number"))
+#if number % 2 == 0:
+      #print("even number")
+#else:
+      #print("odd number")
+#temperature=int(input("enter temperature"))
+#if temperature >38:
+ #     print("high temperature")
+#else:
+ #
+#print("no fever")
+#for i in range(90):
+      #print("Yaa")
+      #print(i)
+#for i in range(15):
+      #print("hello")
