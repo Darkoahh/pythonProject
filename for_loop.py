9cf3eaf4cc53199c17eb4aac5c5c212711766629
Darkoahh <yaakwachie@gmail.com>
2024-07-05 17:01:55 +0000

    commit_message

---
+++ for_loop.py
@@ -0,0 +1,21 @@
+print("hi")
+for i in range(5):
+    print(i)
+for i in range(3):
+    print('something')
+for y in range(2):
+    print("congrats")
+for num in range(2):
+    print("coding is fun")
+for x in range(10):
+    print(x)
+for num in range(6):
+    print("num")
+for tom in range(1):
+    print("love")
+for something in range(7):
+    print("something")
+for sweet in range(4):
+    print("kay")
+    for i in range(56):
+        print("yaa")
