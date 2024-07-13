a6aa6c6d5a64339b874e1929a6fc81f09977c26e
Darkoahh <yaakwachie@gmail.com>
2024-07-13 11:36:44 +0000

    goodness.py

---
+++ goodness.py
@@ -0,0 +1,59 @@
+band = "heartbeat"
+song ="love you"
+song1 = "kiki"
+print(song)
+currency = 'cedis'
+amount = 800
+status = 'overdue'
+print(status)
+current = "cedis"
+amount = 700
+status = "ok"
+print(currency)
+currency = "USD"
+amount = 600
+print(amount)
+campaign="email"
+print(campaign)
+salary = 750
+new_salary = salary + 200
+print(new_salary)
+salary = 800
+new_salary = salary - 300
+print(new_salary)
+salary = 20000
+pay_raise = 400
+new_salary = salary + pay_raise
+print(new_salary)
+paymen_status = "debt"
+payment_status5 = "paid"
+print(payment_status5)
+a = 5
+b = 9
+c = 6
+print(b)
+username = "zombie"
+points = 60
+lives = 4
+print(points)
+print(username)
+print(lives)
+a = "stephen"
+b = "amponsah"
+print(a+b)
+movie = "titanic"
+year = 2005
+print(movie)
+print(year)
+score = 54
+score8 = 4
+print(score)
+print(score8)
+car_type = "sedan"
+print(car_type)
+a = "moon"
+b = "guide"
+c = "honey"
+print(c+a)
+print(c+b)
+print(a+b)
