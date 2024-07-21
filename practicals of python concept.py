71fe9b034c42bf86e8868bf05042c4b9eb07e22b
Darkoahh <yaakwachie@gmail.com>
2024-07-21 15:42:01 +0000

    practicals of python concept .py

---
+++ practicals of python concept.py
@@ -0,0 +1,43 @@
+#car = 5
+#while car > 3:
+   # print("sell cars")
+#for i in range (5):
+    #print("for loop")
+#rank = 200
+#if rank > 90:
+ #print("good day")
+#else:
+ #print("bad day")
+#count = 1
+#while count < 2:
+ #   print(count)
+#heartrate = 60
+#if heartrate < 60:
+ ##elif heartrate >= 60:
+ #print("high heart rate")
+#else:
+ #   print("normal heart rate")
+#seats = 100
+#while seats > 0:
+ #   print("sell ticket")
+  #  seats =seats -1
+#movie = 12
+#while movie > 3:
+ #   print("aladin")
+  #  movie = movie -2
+#song = 25
+#while song > 6:
+ #   print("thank you lord")
+  #  song = song -1
+#counter = 2
+#while counter < 4:
+    #print(counter)
+   # counter = counter +1
+counter = 7
+#while counter > 1:
+ ### counter = counter - 2
+#i = 0
+#while i < 5:
+ #   print(i)
+  #  print("i")
+   # i = i+1
