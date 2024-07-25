b38cf29eadc20c93313f335023012881cc6445ee
Darkoahh <yaakwachie@gmail.com>
2024-07-25 19:32:42 +0000

    practicals on python concept part 2.py

---
+++ practicals on python concept part 2.py
@@ -0,0 +1,41 @@
+age = 45
+print(age)
+name = "yaa"
+salary = 715
+print(name)
+print(salary)
+car = "benz"
+fuel = 150
+print(fuel)
+print(car)
+player = "james"
+score = 5
+level = 12
+print(level)
+print(score)
+print(player)
+movie = "titanic"
+year =1997
+print(movie)
+print(year)
+items = 18
+boxes =6
+print(items/boxes)
+print(items*boxes)
+print(items+boxes)
+print(items-boxes)
+message = "debugging"
+print(message)
+score2 = 45
+score =13
+print(score)
+print(score2)
+movie ="merlin"
+print(movie)
+name =input("ttgfi")
+age =int(input(7))
+is_adult = true
+print(name)
+money =24
+currency ="$"
+print(currency)
